5ba4a6487063b9e8855aa178c6cbf0415f4b816b
MichelleSchoen <70717840+MichelleSchoen@users.noreply.github.com>
2020-09-16 16:16:34 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,35 @@
 // JavaScript Document
+
+// afbeeldingen personen:
+var persoonPlaatjes = ['persoon1.png', 'persoon2.png', 'persoon3.png', 'persoon4.png' ]
+
+var persoonImg = document.querySelector('#persoon');
+
+// toestand
+var beeldfase = 0 ;
+toonPersoon() ;
+
+
+//function eventHandlers
+function toonPersoon() {
+	console.log(beeldfase);
+	
+	// timer:
+	setTimeout( anderPersoon, 4000) 
+	
+	// het tonen van de plaatjes.
+	persoonImg.src = 'images/' + persoonPlaatjes[ beeldfase ];
+}
+
+function anderPersoon() {
+	if (beeldfase < 4 ) {
+		beeldfase = beeldfase + 1 ;
+	} 
+
+	if (beeldfase > 3 ) {
+		beeldfase = 0 ;
+	} 
+
+	
+	toonPersoon() ;
+}
