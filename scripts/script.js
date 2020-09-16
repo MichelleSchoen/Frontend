0866ca3c3174e367061f3098b6a4bcdba787529f
MichelleSchoen <70717840+MichelleSchoen@users.noreply.github.com>
2020-09-16 20:15:19 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,43 @@
 // JavaScript Document
 
 // afbeeldingen personen:
-var persoonPlaatjes = ['persoon1.png', 'persoon2.png', 'persoon3.png', 'persoon4.png' ]
+// var persoonPlaatjes = ['persoon1.png', 'persoon2.png', 'persoon3.png', 'persoon4.png' ]
+var persoonInformatie = 
+[{
+img_url: 'persoon1.png',
+naam:'Kerry F.',
+locatie: 'New York', 
+expertise:'B2B expert',
+increase: '+95%' },
+ 
+{
+img_url: 'persoon2.png',
+naam:'Gemma, R.',
+locatie: 'San Fransisco', 
+expertise:'E-commerce expert',
+increase: '+135%' },
+
+{
+img_url: 'persoon3.png',
+naam:'Eldar',
+locatie: 'Tel Aviv',
+expertise:'Tech expert',
+increase: '+146%' },
+
+{
+img_url: 'persoon4.png',
+naam:'Amy L.',
+locatie: 'Houston',
+expertise:'Health & Beauty expert',
+increase: '+150%' }]
+
+
 
 var persoonImg = document.querySelector('#persoon');
+var persoonNaam = document.querySelector('#persoon-naam')
+var persoonLocatie = document.querySelector('#locatie')
+var persoonExpertise = document.querySelector('#expertise')
+var persoonIncrease = document.querySelector('#sales')
 
 // toestand
 var beeldfase = 0 ;
@@ -18,7 +52,12 @@ function toonPersoon() {
 	setTimeout( anderPersoon, 4000) 
 	
 	// het tonen van de plaatjes.
-	persoonImg.src = 'images/' + persoonPlaatjes[ beeldfase ];
+	persoonInformatie[beeldfase].img_url
+	persoonNaam.textContent = persoonInformatie[beeldfase].naam;
+	persoonLocatie.textContent = persoonInformatie[beeldfase].locatie;
+	persoonIncrease = persoonInformatie[beeldfase].increase;
+	image.src = persoonInformatie[beeldfase].img_url;
+	persoonExpertise.textContent = persoonInformatie[beeldfase].expertise;
 }
 
 function anderPersoon() {
